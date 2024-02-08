327d5ab861043cf4a0d4012e8288074940ab8572
prajwal <prajwalthete98@gmail.com>
2024-02-08 14:51:18 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace a;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome To Sanke And Ladder Simulator Game");
+
+
+    }
+}
