3335b1cabc265b7f9abae804d0cf3b1a54e1beff
prajwal <prajwalthete98@gmail.com>
2024-02-08 14:56:45 +0530

    [PRAJWAL] Add single-player-at-start-position-0

--- Program.cs
+++ Program.cs
@@ -2,12 +2,15 @@
 
 namespace a;
 
+//UC1 - Snake and Ladder game played with single player at start position 0
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome To Sanke And Ladder Simulator Game");
-
+        int PlayerPosition = 0;
+        Console.WriteLine("PlayerPosition is at : " + PlayerPosition);
 
     }
 }
